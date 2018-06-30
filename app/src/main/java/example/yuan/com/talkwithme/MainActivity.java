3b26b9b412a38428968d0c60d8b5b46708dcb94f
yhxxxbl <748704740@qq.com>
2018-06-30 22:12:45 +0800

    编写了初步的界面
    分为home（我的）contact（联系人）find（发现）三个模块

--- app/src/main/java/example/yuan/com/talkwithme/MainActivity.java
+++ app/src/main/java/example/yuan/com/talkwithme/MainActivity.java
@@ -1,13 +1,72 @@
 package example.yuan.com.talkwithme;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import example.yuan.com.common.app.Activity;
+import example.yuan.com.common.widget.PortraitView;
+
+public class MainActivity extends Activity {
+
+    @BindView(R.id.appBar)
+    View mLayAppBar;
+
+    @BindView(R.id.img_portrait)
+    PortraitView mportrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTextView;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.BottomNavigation)
+    BottomNavigationView mNavigation;
+
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        Glide.with(this)
+                .load(R.drawable.bg_src_paint)
+                .centerCrop()
+                .into(new ViewTarget<View,GlideDrawable>(mLayAppBar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+    @OnClick(R.id.img_serch)
+    void onSearchMenuClick(){
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
     }
 }
---
+++ common/src/main/java/example/yuan/com/common/widget/PortraitView.java
@@ -0,0 +1,25 @@
+package example.yuan.com.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Administrator on 2018/6/30/030.
+ */
+
+public class PortraitView extends CircleImageView {
+
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}

f2c2fc6f12d22eeef3beb49fcfa79dcf28a67b06
yuanh <yuanh@fxiaoke.com>
2018-12-07 19:46:58 +0800

    重写自己的DrawerLayout以便实现全局侧滑
    重写了onInterceptMotionEvent便于监听事件

--- app/src/main/java/example/yuan/com/talkwithme/activities/MainActivity.java
+++ app/src/main/java/example/yuan/com/talkwithme/activities/MainActivity.java
@@ -1,12 +1,24 @@
 package example.yuan.com.talkwithme.activities;
 
+import java.util.Objects;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.gifdecoder.GifDecoder;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,28 +27,18 @@ import android.view.WindowManager;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.ViewTarget;
-
-import net.qiujuer.genius.ui.Ui;
-
-import java.util.Objects;
-
 import butterknife.BindView;
 import butterknife.OnClick;
 import example.yuan.com.common.app.Activity;
+import example.yuan.com.common.widget.DrawerLayout;
 import example.yuan.com.common.widget.PortraitView;
 import example.yuan.com.talkwithme.R;
 import example.yuan.com.talkwithme.fragments.main.ActiveFragment;
 import example.yuan.com.talkwithme.fragments.main.ContactFragment;
 import example.yuan.com.talkwithme.fragments.main.FindFragment;
 import example.yuan.com.talkwithme.helper.NavHelper;
+import net.qiujuer.genius.ui.Ui;
 
 public class MainActivity extends Activity implements
         BottomNavigationView.OnNavigationItemSelectedListener, NavHelper.OnTabChangedListener<Integer> {
@@ -90,8 +92,7 @@ public class MainActivity extends Activity implements
     @Override
     protected void initWidget() {
         super.initWidget();
-        NavigationView navigationView = findViewById(R.id.main_layout_left);
-        View mHeaderLyaout = navigationView.getHeaderView(0)
+        View mHeaderLayout = mLayoutLeft.getHeaderView(0)
                 .findViewById(R.id.header_layout);
         //初始化底部辅助工具类
         mNavHelper = new NavHelper<>(this, R.id.lay_container, getSupportFragmentManager(), this);
@@ -103,17 +104,9 @@ public class MainActivity extends Activity implements
         //添加事件监听
         mNavigation.setItemIconTintList(null);
         mNavigation.setOnNavigationItemSelectedListener(this);
-        Glide.with(this)
-                .load(R.drawable.bg_src_paint)
-                .centerCrop()
-                .into(new ViewTarget<View, GlideDrawable>(mLayAppBar) {
-                    @Override
-                    public void onResourceReady(GlideDrawable resource,
-                                                GlideAnimation<? super GlideDrawable> glideAnimation) {
-                        this.view.setBackground(resource.getCurrent());
-                    }
-                });
         //为DrawerLayout设置监听事件
+
+        mDrawerLayout.setCustomLeftEdgeSize(mDrawerLayout,1f);
         mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
@@ -123,6 +116,7 @@ public class MainActivity extends Activity implements
                 //设置右面的布局位置  根据左面菜单的right作为右面布局的left   左面的right+屏幕的宽度（或者right的宽度这里是相等的）为右面布局的right
                 mLayoutRight.layout(mLayoutLeft.getRight(), 0, mLayoutLeft.getRight() + display.getWidth(),
                         display.getHeight());
+
             }
 
             @Override
@@ -149,20 +143,59 @@ public class MainActivity extends Activity implements
                 return true;
             }
         });
-        Glide.with(this)
-                .load(R.drawable.header_photo)
+
+        Glide.with(this)//加载ToolBar的图片
+                .load(R.drawable.bg_src_paint)
                 .centerCrop()
-                .into(new ViewTarget<View, GlideDrawable>(mHeaderLyaout) {
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppBar) {
                     @Override
                     public void onResourceReady(GlideDrawable resource,
                                                 GlideAnimation<? super GlideDrawable> glideAnimation) {
                         this.view.setBackground(resource.getCurrent());
                     }
                 });
-        Glide.with(this)
-                .load(R.drawable.nav_drawer_img)
+
+        Glide.with(this)//加载NavigationView中HeaderLayout的背景
+                .load(R.drawable.header_photo)
                 .centerCrop()
-                .into(mNavImageView);
+                .into(new ViewTarget<View, GlideDrawable>(mHeaderLayout) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource,
+                                                GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+
+        Glide.with(getApplicationContext())
+                .load(R.drawable.nav_drawer_img)//加载厘米秀
+                .listener(new RequestListener<Integer, GlideDrawable>() {
+                              @Override
+                              public boolean onException(Exception e, Integer model
+                                      , Target<GlideDrawable> target, boolean isFirstResource) {
+                                  return false;
+                              }
+
+                              @Override
+                              public boolean onResourceReady(GlideDrawable resource, Integer model,
+                                                             Target<GlideDrawable> target,
+                                                             boolean isFromMemoryCache, boolean
+                                                                     isFirstResource) {
+                                  // 获取gif动画时长
+                                  GifDrawable drawable = (GifDrawable) resource;
+                                  GifDecoder decoder = drawable.getDecoder();
+                                  long duration = 0;
+                                  for (int i = 0; i < drawable.getFrameCount(); i++) {
+                                      duration += decoder.getDelay(i);
+                                  }
+                                  Log.e("peter", "动画时长" + duration);
+                                  //后面这段代码根据你的具体业务需求，添加相应的代码块
+              /*  handler.sendEmptyMessageDelayed(MESSAGE_SUCCESS,
+                        duration + 700);*/
+                                  return false;
+                              }
+                          }
+                ).into(new GlideDrawableImageViewTarget
+                (mNavImageView, 1));//1代表播放一次
     }
 
     @Override
